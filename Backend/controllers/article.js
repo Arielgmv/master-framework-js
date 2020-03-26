d09f3ca7f7060ed1f533201a72b95e9cae1f872d
Arielgmv <ariel.munoz.villegas@gmail.com>
2020-03-26 18:11:26 -0400

    Avances_(26-3-20)

--- Backend/controllers/article.js
+++ Backend/controllers/article.js
@@ -21,7 +21,7 @@ var controller = {
         });
     },
 
-    save: (req, res) =>{
+    save: (req, res) =>{ //método save, permitira crear un nuevo articulo
         //Recoger parámetros por POST
         var params = req.body; //vamos a recoger lo que nos llegue por el body
         //console.log(params);
@@ -31,29 +31,77 @@ var controller = {
             var validate_title = !validator.isEmpty(params.title);//validate_title dara true cuando params.title no este vacio             
             var validate_content = !validator.isEmpty(params.content);
         } catch (error) {
-            return res.status(200).send({                
+            return res.status(200).send({  
+                status: 'error', 
                 message: 'Faltan datos por enviar'
             }); 
         }
         if(validate_title && validate_content){
             //Crear el objeto a guardar
+            var article = new Article();//instanciamos el objeto del modelo
 
             //Asignar valores al objeto
+            article.title = params.title;
+            article.content = params.content;
+            article.image = null;
 
             //Guardar el artículo
-
-            //Devolver una respuesta
-
-            return res.status(200).send({
-                article: params
-            });
+            article.save((err, articleStored) => {
+                if (err || !articleStored) {
+                    return res.status(404).send({
+                        status: 'error',
+                        message: 'El artículo no se ha guardado'
+                    });
+                }
+                //Devolver una respuesta
+                return res.status(200).send({
+                status: 'success',
+                article: articleStored//ya que se asigno valores al objeto, devolvemos solo el article
+                //article: params
+                });
+            });//utilizamos el metodo save que tiene varias caracteristicas, pasamos el parametro err (error)
+            //en caso de que suceda, en caso de que se guarde nos devolvera esta variable relleno articleStored
         }else{
             return res.status(200).send({
+                status: 'error',
                 message: 'Los datos no son validos'
             });
         }
         
-    }//método save, permitira crear un nuevo articulo
+    },
+
+    getArticles: (req, res) => { //metodo getArticles
+        
+        var query = Article.find({});
+
+        var last = req.params.last;
+        if (last || last != undefined) {
+            query.limit(5); //colocamos un limit a la query que estamos haciendo
+        }
+        
+        //Find (para sacar los datos de la BBDD)
+        query.sort('-_id').exec((err, articles) => { //sin condiciones, por que quiero sacarlo todo, metodo sort para ordenar de manera descendente (-), utilizo el método exec
+            if (err) {
+                return res.status(500).send({
+                    status: 'error',
+                    message: 'Error al devolver los artículos'
+                });    
+            }
+            
+            if (!articles) {
+                return res.status(404).send({
+                    status: 'error',
+                    message: 'No hay artículos para mostrar'
+                });    
+            }
+            
+            return res.status(200).send({
+                status: 'success',
+                articles
+            });
+        }); 
+    }
+
 };//end controller 
 
 //utilizamos el module export para poder utilizar este objeto fuera de este archivo y 
--- Backend/routes/article.js
+++ Backend/routes/article.js
@@ -11,6 +11,7 @@ router.get('/test-de-controlador', ArticleController.test);//creo una ruta por g
 
 //Rutas útiles
 router.post('/save', ArticleController.save);
+router.get('/articles/:last?', ArticleController.getArticles);//? parámetro opcional por la URL
 
 //exporto el modulo para poder usarlo
 module.exports = router;
